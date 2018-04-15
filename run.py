3e3ced0776a0f4d96d6b3c448d1a06f9ada3e3b9
Ben Hasselgren <ben.nadalito@gmail.com>
2018-04-15 23:40:33 +0000

    Added contact form that posts info to server

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,11 +16,22 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company_data=data)
+    return render_template("about.html", page_title="About", company=data)
+    
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj['url'] == member_name:
+                member = obj
+    return render_template("members.html", member=member)
 
-
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form)
     return render_template("contact.html", page_title="Contact")
 
 
